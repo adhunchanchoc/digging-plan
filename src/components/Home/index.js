13d64b55ee4cf0cdfa27a3834a5123fcaa29fe8c
adhunchanchoc <adhunchanchoc@gmail.com>
2023-03-28 13:51:20 +0200

    taby bez funkcnosti

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { PageWrap, List, Item, Tabs, TabBtn } from "./styledComps";
 
 export default function Home() {
   return (
     <>
-      <h3>Testing home component</h3>
+      <PageWrap>
+        <Tabs>
+          <TabBtn>Zamestnanci</TabBtn>
+          <TabBtn>Zadane ukoly</TabBtn>
+        </Tabs>
+
+        <h3>Zamestnanci</h3>
+        <List>
+          <Item>kop</Item>
+          <Item>kop</Item>
+          <Item>kop</Item>
+        </List>
+
+        <h3>Zadane ukoly</h3>
+        <List>
+          <Item>kop</Item>
+          <Item>kop</Item>
+          <Item>kop</Item>
+        </List>
+      </PageWrap>
     </>
   );
 }
---
+++ src/components/Home/styledComps.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const PageWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* align-items: space-between; */
+  /* text-align: center; */
+  border: 1px blue solid;
+  max-width: 700px;
+  margin: 0 auto;
+`;
+export const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px black solid;
+`;
+export const Item = styled.div`
+  color: grey;
+  display: block;
+  border: 1px red solid;
+  font-size: 20px;
+  padding: 0.2rem;
+`;
+
+export const Tabs = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  flex-wrap: wrap;
+  /* justify-content: space-around; */
+  /* align-items: stretch; */
+`;
+export const TabBtn = styled.button`
+  flex-basis: 160px;
+  flex-grow: 1;
+  font-size: 16px;
+  font-weight: bold;
+  width: 100%;
+  height: 45px;
+`;
